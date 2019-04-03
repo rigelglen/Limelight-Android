4af596db7a5f927a2b7f9989dd7afce3bc62e74e
Rigel Glen <rigelglen@gmail.com>
2019-04-03 19:49:02 +0530

    Moved to material components and created design for classify fragment

--- app/src/main/java/com/limelight/limelight/fragments/ClassifyFragment.java
+++ app/src/main/java/com/limelight/limelight/fragments/ClassifyFragment.java
@@ -1,8 +1,31 @@
 package com.limelight.limelight.fragments;
 
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.limelight.limelight.R;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class ClassifyFragment extends Fragment {
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_classify, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
 }
--- app/src/main/java/com/limelight/limelight/fragments/FeedFragment.java
+++ app/src/main/java/com/limelight/limelight/fragments/FeedFragment.java
@@ -44,14 +44,10 @@ public class FeedFragment extends Fragment {
     private ArrayList<Article> feedArticles;
     private SharedPreferences sharedPref;
     private SwipeRefreshLayout swipeContainer;
-    int pastVisiblesItems, visibleItemCount, totalItemCount;
 
-
-    private Api api = RetrofitClient.getInstance().getApiService();
     private int page = 1;
     private String token = "";
-    private static boolean loadMore = true;
-    FeedViewModel model;
+    private FeedViewModel model;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -64,15 +60,14 @@ public class FeedFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_feed, container, false);
     }
 
-
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        feedArticles = new ArrayList<Article>();
-        api = RetrofitClient.getInstance().getApiService();
+        feedArticles = new ArrayList<>();
 
         swipeContainer = view.findViewById(R.id.feedContainer);
+        RecyclerView recyclerView = view.findViewById(R.id.feed_recycler);
 
         swipeContainer.setRefreshing(true);
 
@@ -87,8 +82,6 @@ public class FeedFragment extends Fragment {
             logout();
         }
 
-        model = ViewModelProviders.of(getActivity()).get(FeedViewModel.class);
-
         model.getArticles(token, page, getActivity(), false, false).observe(this, articleList -> {
             feedArticles.clear();
             feedArticles.addAll(articleList);
@@ -96,25 +89,18 @@ public class FeedFragment extends Fragment {
             swipeContainer.setRefreshing(false);
         });
 
-
-        //feedArticles.add(new Article("title1 this is a very long title, very ling title, test test", "source1", "desc1", "url1", "imgUrl1", 123456));
-        //feedArticles.add(new Article("title2", "source2", "desc2", "url2", "imgUrl1", 123456));
-
-
-        RecyclerView recyclerView = view.findViewById(R.id.feed_recycler);
         RecyclerViewClickListener listener = (v, position) -> {
             Intent i = new Intent(getActivity(), ViewArticleActivity.class);
             i.putExtra("url", feedArticles.get(position).getLink());
             startActivity(i);
         };
 
-
         // use a linear layout manager
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
         recyclerView.setLayoutManager(layoutManager);
 
         // specify an adapter (see also next example)
-        mAdapter = new FeedAdapter(feedArticles,listener);
+        mAdapter = new FeedAdapter(feedArticles, listener);
         recyclerView.setAdapter(mAdapter);
 
         swipeContainer.setOnRefreshListener(() -> {
@@ -127,140 +113,8 @@ public class FeedFragment extends Fragment {
             });
         });
 
-//        //code to set a onclick listener on each item of recycler view
-//        recyclerView.addOnItemTouchListener(new RecyclerItemClickListener(getContext(), recyclerView, new RecyclerItemClickListener.OnItemClickListener(){
-//
-//            @Override
-//            public void onItemClick(View view, int position) {
-//                RelativeLayout article = view.findViewById(R.id.article);
-//                article.setOnClickListener(v->{
-//                    //go to intent
-//                    Intent i = new Intent(getActivity(), ViewArticleActivity.class);
-//                    startActivity(i);
-//
-//                });
-//            }
-//
-//            @Override
-//            public void onItemLongClick(View view, int position) {
-//
-//            }
-//        }));
+        /* TODO Fix scrolling */
 
-
-
-
-
-
-
-
-                LinearLayoutManager linearLayoutManager = ((LinearLayoutManager) recyclerView.getLayoutManager());
-        // TODO Fix scrolling
-//        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
-//            @Override
-//            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
-//
-//                visibleItemCount = linearLayoutManager.getChildCount();
-//                totalItemCount = linearLayoutManager.getItemCount();
-//                pastVisiblesItems = linearLayoutManager.findFirstVisibleItemPosition();
-//
-//                if (loadMore) {
-//                    if ((visibleItemCount + pastVisiblesItems) >= totalItemCount) {
-//                        if (page <= 4) {
-//                            page++;
-//                            loadMore = false;
-//                            Toast.makeText(getContext(), "Loading more...", Toast.LENGTH_SHORT).show();
-//                            model.getArticles(token, page, getActivity(), true, true).observe(FeedFragment.this, articleList -> {
-//                                feedArticles.addAll(articleList);
-//                                mAdapter.notifyDataSetChanged();
-//                                if(articleList.size()!=0)
-//                                    loadMore = true;
-//                            });
-//                        }
-//                    }
-//                }
-//            }
-//        });
-
-
-    }
-
-    public void getFeed() {
-        //make network request to obtain feed
-        //HashMap<String,String> map = new HashMap<>();
-
-        String token = "";
-        sharedPref = getActivity().getSharedPreferences("limelight", Context.MODE_PRIVATE);
-
-        if (sharedPref.contains("token")) {
-            //get token from sharedprefs
-            token = "Bearer " + sharedPref.getString("token", "");
-            Log.i("token11", token);
-        } else {
-            //go to login activity
-            logout();
-        }
-
-        if (!token.equals("")) {
-
-//            HashMap<String, Integer> map = new HashMap<>();
-//            map.put("page", page);
-            Call<ArrayList<Article>> call = api.getFeed(token, page);
-
-
-            call.enqueue(new Callback<ArrayList<Article>>() {
-                @Override
-                public void onResponse(@NonNull Call<ArrayList<Article>> call, @NonNull Response<ArrayList<Article>> response) {
-                    if (response.body() != null && response.isSuccessful()) {
-
-                        //get array of articles
-                        feedArticles.clear();
-                        feedArticles.addAll(new ArrayList<>(response.body()));
-                        //feedArticles=new ArrayList<>(response.body());
-                        Log.i("abcd", feedArticles.toString());
-                        mAdapter.notifyDataSetChanged();
-
-                    } else if (response.errorBody() != null) {
-                        Gson gson = new GsonBuilder().create();
-                        ErrorModel mErrorModel;
-                        try {
-                            mErrorModel = gson.fromJson(response.errorBody().string(), ErrorModel.class);
-                            //Toast.makeText(getApplicationContext(), mErrorModel.getMessage(), Toast.LENGTH_LONG).show();
-                            new SweetAlertDialog(getActivity(), SweetAlertDialog.ERROR_TYPE)
-                                    .setTitleText("Error")
-                                    .setContentText(mErrorModel.getMessage())
-                                    .show();
-
-                            //logout();
-
-                        } catch (IOException e) {
-                            Toast.makeText(getActivity(), "An error occurred", Toast.LENGTH_LONG).show();
-                        }
-                    } else {
-                        Toast.makeText(getActivity(), "An error occurred", Toast.LENGTH_LONG).show();
-                    }
-
-
-                }
-
-                @Override
-                public void onFailure(@NonNull Call<ArrayList<Article>> call, @NonNull Throwable t) {
-
-
-                    SweetAlertDialog pDialog = new SweetAlertDialog(getActivity(), SweetAlertDialog.ERROR_TYPE)
-                            .setTitleText("Error")
-                            .setContentText("No internet connection");
-                    pDialog.show();
-                }
-
-            });
-
-
-        } else {
-            //go to login activity
-            logout();
-
-        }
     }
 
     private void logout() {
