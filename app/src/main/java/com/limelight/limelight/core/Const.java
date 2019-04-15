6629f39faa2bbf31f73b191832ef25f1995d7a13
JyothyMiranda <32127771+JyothyMiranda@users.noreply.github.com>
2019-04-15 14:56:35 +0530

    added server url

--- app/src/main/java/com/limelight/limelight/core/Const.java
+++ app/src/main/java/com/limelight/limelight/core/Const.java
@@ -1,5 +1,5 @@
 package com.limelight.limelight.core;
 
 final class Const {
-    static final String BASE_URL = "http://192.168.137.208:4000";
+    static final String BASE_URL = "http://139.59.27.192";
 }
